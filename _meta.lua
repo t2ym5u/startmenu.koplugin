dfaf6163975154b2f60fb82519a2c7e7d33bf6ea
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-17 08:58:30 +0200

    chore: bump version to 1.1.2

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name        = "startmenu",
     fullname    = _("Startup Menu"),
     description = _([[Shows a choice screen on KOReader startup: read or launch a game.]]),
-    version     = "1.1.1",
+    version     = "1.1.2",
 }
