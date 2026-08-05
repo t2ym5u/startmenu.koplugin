99b71b6e47f18bca697abb82ecb3ee8c999748e2
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:23 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,9 +1,9 @@
 return {
-    ["%s is not installed or not active."] = { fr = "%s n'est pas installé ou actif." },
-    ["Enable on startup"]                  = { fr = "Activer au démarrage" },
-    ["Games to show:"]                     = { fr = "Jeux à afficher :" },
-    ["Read"]                               = { fr = "Lire" },
-    ["Show startup menu now"]              = { fr = "Afficher le menu de démarrage maintenant" },
-    ["Startup Menu"]                       = { fr = "Menu de démarrage" },
-    ["What would you like to do?"]         = { fr = "Que souhaitez-vous faire ?" },
+    ["%s is not installed or not active."] = { fr = "%s n'est pas installé ou actif.", es = "%s no está instalado o no está activo.", de = "%s ist nicht installiert oder nicht aktiv." },
+    ["Enable on startup"]                  = { fr = "Activer au démarrage", es = "Activar al inicio", de = "Beim Start aktivieren" },
+    ["Games to show:"]                     = { fr = "Jeux à afficher :", es = "Juegos a mostrar:", de = "Anzuzeigende Spiele:" },
+    ["Read"]                               = { fr = "Lire", es = "Leer", de = "Lesen" },
+    ["Show startup menu now"]              = { fr = "Afficher le menu de démarrage maintenant", es = "Mostrar el menú de inicio ahora", de = "Startmenü jetzt anzeigen" },
+    ["Startup Menu"]                       = { fr = "Menu de démarrage", es = "Menú de inicio", de = "Startmenü" },
+    ["What would you like to do?"]         = { fr = "Que souhaitez-vous faire ?", es = "¿Qué te gustaría hacer?", de = "Was möchtest du tun?" },
 }
