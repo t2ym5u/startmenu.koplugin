34d3d9d3727a7a4f627d4ed0a32929b00dea356c
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 15:07:18 +0200

    chore: bump version to 1.1.11

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Startup Menu"),
     description = _([[Shows a choice screen on KOReader startup: read or launch a game.]]),
-    version     = "1.1.10",
+    version     = "1.1.11",
 }
